c5ffe7df88c6235489c9edafb3304516fc02fbf6
Cynthia Nikolai <cynthianikolai@gmail.com>
2023-06-05 21:33:28 -0400

    Added a cancel button on the forms

--- jokes/views.py
+++ jokes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView, UpdateView)
 from .models import Joke
+from django.urls import reverse_lazy
 
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    fields = ['question', 'answer'] # two fields that are presented to the user for creation
+
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
 
 class JokeDetailView(DetailView):
     model = Joke
@@ -14,4 +19,4 @@ class JokeListView(ListView):
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    fields = ['question', 'answer'] # two fields that are presented to the user for update
